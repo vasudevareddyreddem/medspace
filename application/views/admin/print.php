8d2a91cba2f52934e259b8281d467e32ce37a2ee
sivaksr <40159306+sivaksr@users.noreply.github.com>
2018-09-20 17:57:05 +0530

    barcode hcf
    barcode

--- application/views/admin/print.php
+++ application/views/admin/print.php
@@ -2,8 +2,8 @@
                        
 				<div   style="width:189px;height:189px;border:1px solid #ddd;overflow:hidden;">
 					<div style=" ;padding:10px;">
-					 <div  style="margin:3px 20px">Bio Medical Waste</div>
-					<div style="margin:3px 8px;font-size:15px">Hospital Name:<strong><?php echo $hospital_detail['hospital_name']; ?></strong></div> 		 
+					 <div  style="margin:3px 10px">Bio Medical Waste</div>
+					<div style="margin:3px 10px;font-size:15px">Hospital Name:<strong><?php echo $hospital_detail['hospital_name']; ?></strong></div> 		 
 										
 							 <div class="" id="printThis<?php echo htmlentities($hospital_detail['h_id']); ?>">
 								<img style="width:auto; height:auto;margin-right:30px;" src="<?php echo base_url('assets/hospital_barcodes/'.$hospital_detail['barcode']);?>" alt="bar code">
