91dcbe234ee692321b0971100a490834cd486592
vasudevareddy <vasudevareddy549@gmail.com>
2019-03-17 18:45:47 +0530

    adding popup changes
    adding popup changes

--- application/views/admin/govt/overall_hospital_waste_image.php
+++ application/views/admin/govt/overall_hospital_waste_image.php
@@ -28,21 +28,39 @@
                                     <?php if(isset($waste_imgs) && count($waste_imgs)>0){ ?>
 
 									    <tbody>
-									<?php foreach($waste_imgs as $list){ ?>
+									<?php $cnt=1;foreach($waste_imgs as $list){ ?>
                                 
                                         <tr>
 											<td><?php echo htmlentities($list['hospital_name']); ?></td>
 										  <td><?php echo htmlentities($list['text']); ?></td>
 										  <td title="<?php echo htmlentities($list['address']); ?>"><?php echo substr($list['address'], 0, 15); ?>...</td>
-										 <td data-toggle="modal" data-target="#myModal"><img height="50px;" width="50px;" src="<?php echo base_url('assets/hospital-waste-images/'.$list['image']); ?>"></td>
+										 <td data-toggle="modal" data-target="#myModal<?php echo $cnt; ?>"><img height="50px;" width="50px;" src="<?php echo base_url('assets/hospital-waste-images/'.$list['image']); ?>"></td>
 										 <td><?php echo htmlentities($list['create_at']); ?></td>
 										
                                             
                                             
                                         </tr>
-										
+										<div class="modal fade" id="myModal<?php echo $cnt; ?>" role="dialog">
+											<div class="modal-dialog">
+											
+											  <!-- Modal content-->
+											  <div class="modal-content">
+												<div class="modal-header">
+												  <button type="button" class="close" data-dismiss="modal">&times;</button>
+												  <h4 class="modal-title"><?php echo htmlentities($list['hospital_name']); ?> Waste Image</h4>
+												</div>
+												<div class="modal-body">
+												  <img class="img-responsive" src="<?php echo base_url('assets/hospital-waste-images/'.$list['image']); ?>">
+												</div>
+												<div class="modal-footer">
+												  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+												</div>
+											  </div>
+											  
+											</div>
+										  </div>
                                 
-									<?php } ?>
+									<?php $cnt++;} ?>
 									    </tbody>
 									<?php } ?>
                                 </table>
@@ -54,27 +72,7 @@
             <!-- #END# Exportable Table -->
         </div>
 </section>
-<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">HCF Waste Image</h4>
-        </div>
-        <div class="modal-body">
-			<?php foreach($waste_imgs as $list){ ?>
-          <img class="img-responsive" src="<?php echo base_url('assets/hospital-waste-images/'.$list['image']); ?>">
-		  <?php } ?>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-      
-    </div>
-  </div>
+
 	<script type="text/javascript">
    $(document).ready(function() {
     $('#js-date').datepicker(
