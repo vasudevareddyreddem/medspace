35f48f9eeaa5aa2d82961816ee0af08927b37c4d
bayapucg <bayapu.edu004@gmail.com>
2019-08-18 12:28:08 +0530

    fdsafdas
    fdasfdsf

--- application/views/admin/38x25.php
+++ application/views/admin/38x25.php
@@ -40,7 +40,7 @@
 	margin:0 auto;
 }
 </style>
-<div  style="width:100%;height:100%;text-align:center;margin-top:0.5cm;">
+<div  style="width:100%;height:100%;text-align:center;margin-top:0.8cm;">
 <?php if(isset($print_details) && count($print_details)>0){ ?>
 <?php foreach($print_details as $list){ ?>
 		<div class="loop" style="margin-left:3mm">
