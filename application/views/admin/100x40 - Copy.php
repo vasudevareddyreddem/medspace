7b1bce764c14c5a9203726fb198b59d8952db777
bayapucg <bayapu.edu004@gmail.com>
2019-08-23 10:50:50 +0530

    commit
    changes

--- application/views/admin/100x40 - Copy.php
+++ application/views/admin/100x40 - Copy.php
@@ -7,90 +7,57 @@
 .loop > .sticker{
 	float:left;
 }
-.yellow-col{
-	background-color:yellow;
-	width:30px;
-	height:30px;
-	margin:0 auto;
-}
-.red-col{
-	background-color:red;
-	width:30px;
-	height:30px;
-	margin:0 auto;
-}
-.blue-col{
-	background-color:blue;
-	width:30px;
-	height:30px;
-	margin:0 auto;
-}
-.white-col{
-	background-color:f5f5f5;
-	width:30px;
-	height:30px;
-	margin:0 auto;
-
 </style>
 <div  style="width:210mm;height:297mm;text-align:center;">
 <?php if(isset($print_details) && count($print_details)>0){ ?>
 <?php foreach($print_details as $list){ ?>
 		<div class="loop" style="margin:0 2mm;">
-		<div class="sticker" style="width:100mm;height:40mm;background:#fff;border:1px solid #ddd;margin-top:1mm;position:relative">
+		<div class="sticker" style="width:100mm;height:40mm;background:#fff;border:1px solid #ddd;margin-top:1mm">
 				<div >
-					<h3 style="padding:0px;margin:0px;">RM11029DL00001</h3>
+					<h3 style="padding:0px;margin:0px;">Bio-medical waste barcode</h3>
 					<img style="width:auto;height:15mm;padding:5px 0px;" src="<?php echo base_url('assets/hospital_waste_barcodes/'.$list[0]['barcode']); ?>" alt="<?php echo $list[0]['barcode'] ; ?>" >
 				</div>
-				<div style="position: absolute;;top:10px;left:10px">
-					<div class="blue-col"></div>
-					<div style="font-size:8px">DRRML110065DLBH00000</div>
-				</div>
-				
 				<div class="">
 					<div>
 						<table style="width:100%;padding:0px 20px;font-size:14px;line-height:15px;" align="center">
 							<tbody>
-								<!--<tr>
+								<tr>
 									<td style="">Category</td>
 									<td ><span><?php echo $list[0]['category'] ; ?> </span>bag</td>
-								</tr>-->
+								</tr>
 								<tr>
 									<td>HCF</td>
-									<td>: <span><?php echo $list[0]['h_name'] ; ?> </span></td>
+									<td><span><?php echo $list[0]['h_name'] ; ?> </span></td>
 								</tr>
 								<tr>
 									<td>CBWTF</td>
-									<td>: <?php echo $list[0]['cbwtf'] ; ?></td>
+									<td><?php echo $list[0]['cbwtf'] ; ?></td>
 								</tr>
 							</tbody>
 						</table>
 					</div>
 				</div>
 		</div>
-		<div class="sticker" style="width:100mm;height:40mm;background:#fff;border:1px solid #ddd;margin-left:3mm;margin-top:1mm;position:relative">
+		<div class="sticker" style="width:100mm;height:40mm;background:#fff;border:1px solid #ddd;margin-left:3mm;margin-top:1mm">
 				<div >
-					<h3 style="padding:0px;margin:0px;">RM11029DL00001</h3>
+					<h3 style="padding:0px;margin:0px;">Bio-medical waste barcode</h3>
 					<img style="width:auto;height:15mm;padding:5px 0px;" src="<?php echo base_url('assets/hospital_waste_barcodes/'.$list[1]['barcode']); ?>" alt="Barcode" >
-				</div>
-				<div style="position: absolute;;top:10px;left:10px">
-					<div class="blue-col"></div>
-					<div style="font-size:8px">DRRML110065DLBH00000</div>
 				</div>
 				<div class="">
 					<div>
 						<table style="width:100%;padding:0px 20px;font-size:14px;line-height:15px;" align="center">
 							<tbody>
-								<!--<tr>
+								<tr>
 									<td style="">Category</td>
 									<td ><span><?php echo $list[1]['category'] ; ?> </span>bag</td>
-								</tr>-->
+								</tr>
 								<tr>
 									<td>HCF</td>
-									<td>: <span><?php echo $list[1]['h_name'] ; ?>
+									<td><span><?php echo $list[1]['h_name'] ; ?>
 								</tr>
 								<tr>
 									<td>CBWTF</td>
-									<td>: <?php echo $list[1]['cbwtf'] ; ?></td>
+									<td><?php echo $list[1]['cbwtf'] ; ?></td>
 								</tr>
 							</tbody>
 						</table>
