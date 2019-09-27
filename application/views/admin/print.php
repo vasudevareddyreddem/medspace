1f5b338795b7e06b8bce2bcf4abc6e089ef15773
bayapucg <bayapu.edu004@gmail.com>
2019-09-27 12:54:23 +0530

    COMMIT
    CAHGNES

--- application/views/admin/print.php
+++ application/views/admin/print.php
@@ -5,7 +5,7 @@
 	font-family:arial;
 }
 .barcode-div img{
-	height:4.8cm;
+	height:2.8cm;
 }
 .sticker-logo {
 	
@@ -13,22 +13,23 @@
 	padding-bottom:0.1cm;
 }
 .sticker-logo img{
-	height:1.5cm;
-	margin:0 auto;
+	margin-top: 2px;
+	height:1.2cm;
+	// margin:0 auto;
 	text-align:center;
 }
 .hosptal_name{
 	text-align:center;
 	font-size:17px;
 	letter-spaci1ng:0.2px;
-	min-height:1cm;
+	min-height:1.5cm;
 	}
 .hosptal_name1{
 	text-align:center;
 	font-size:17px;
 	letter-spacing:0.2px;
 	
-	min-height:1.5cm;
+	min-height:1cm;
 }
 .qr-code{
 	text-align:center;
@@ -60,11 +61,24 @@
 	font-size:10px;
 	text-align:center
 }
+.loop-help{
+	display:flex;
+	margin-top:0.65cm;
+	
+}
+.sticker-row{
+	margin:0 1.33cm ;
+	
+}
+.sticker-row1{
+	margin:0 1.33cm 0 0 ;
+	
+} 
 </style>
 <div style="width:21cm;height:29.7cm;overflow:hidden;">
-	<div class="row" style="position:relative;margin-top:1.9cm">
-		<div style="width:8.5cm;height:12cm;border:1px solid #aaa;overflow:hidden;margin-left:1.33cm">
-			<div style="padding:20px 5px;position: relative;">
+	<div class="row loop-help" style="position:relative;">
+		<div class="sticker-row" style="width:8.5cm;height:9cm;border:1px solid #aaa;overflow:hidden;">
+			<div style="position: relative;">
 			<div class="hazard">
 				<img style="" src="<?php echo base_url('assets/vendor/images/biohazard.png');?>" alt="bar code" />
 			</div>
@@ -93,9 +107,7 @@
 				<div class="hosptal_name1">
 					<?php echo $hospital_detail['hospital_name']; ?> 
 					<?php echo ', '.$hospital_detail['city']; ?> 
-					
 				</div>
-				
 				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
 				
 				<div class="address-section">
@@ -109,11 +121,239 @@
 				<div class="address-section-con">
 				<?php echo isset($plant_details['mobile'])?$plant_details['mobile']:''; ?> | <?php echo isset($plant_details['email'])?$plant_details['email']:''; ?>
 				</div>
-				
+			</div>
+		</div><div class="sticker-row1" style="width:8.5cm;height:9cm;border:1px solid #aaa;overflow:hidden;">
+			<div style="position: relative;">
+			<div class="hazard">
+				<img style="" src="<?php echo base_url('assets/vendor/images/biohazard.png');?>" alt="bar code" />
+			</div>
+			<div class="sticker-logo">
+			<?php if($plant_details['logo']!=''){ ?>
+			<img src="<?php echo base_url('assets/plant_logo/'.$plant_details['logo']); ?>">
+			<?php }else{ ?>
+			<img src="<?php echo base_url('assets/plant_logo/noimage.jpg'); ?>">
+			<?php } ?>
 				
 			</div>
+			<div class="hosptal_name">
+				<?php echo $plant_details['disposal_plant_name']; ?> 
+				<div class="authorised">
+					(Authorised by <?php echo isset($plant_details['state'])?$plant_details['state']:' '; ?>. Pollution Control Board)
+				</div>
+			</div>
+			
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+			<div class="qr-code">
+				Bio-Medical Waste QR code
+			</div>
+				<div class="barcode-div" style="text-align:center" id="printThis<?php echo htmlentities($hospital_detail['h_id']); ?>">
+					<img style="" src="<?php echo base_url('assets/hospital_barcodes/'.$hospital_detail['barcode']);?>" alt="bar code" />
+				</div>
+				<div class="hosptal_name1">
+					<?php echo $hospital_detail['hospital_name']; ?> 
+					<?php echo ', '.$hospital_detail['city']; ?> 
+				</div>
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+				
+				<div class="address-section">
+				<?php echo isset($plant_details['address1'])?$plant_details['address1'].',':''; ?>
+				<?php echo isset($plant_details['address2'])?$plant_details['address2'].',':''; ?>
+				<?php echo isset($plant_details['city'])?$plant_details['city'].',':''; ?>
+				<?php echo isset($plant_details['state'])?$plant_details['state'].',':''; ?>
+				<?php echo isset($plant_details['country'])?$plant_details['country'].',':''; ?>
+				<?php echo isset($plant_details['pincode'])?$plant_details['pincode'].'.':''; ?>
+				</div>
+				<div class="address-section-con">
+				<?php echo isset($plant_details['mobile'])?$plant_details['mobile']:''; ?> | <?php echo isset($plant_details['email'])?$plant_details['email']:''; ?>
+				</div>
+			</div>
 		</div>
-		
+	</div>
+	<div class="row loop-help" style="position:relative;">
+		<div class="sticker-row" style="width:8.5cm;height:9cm;border:1px solid #aaa;overflow:hidden;">
+			<div style="position: relative;">
+			<div class="hazard">
+				<img style="" src="<?php echo base_url('assets/vendor/images/biohazard.png');?>" alt="bar code" />
+			</div>
+			<div class="sticker-logo">
+			<?php if($plant_details['logo']!=''){ ?>
+			<img src="<?php echo base_url('assets/plant_logo/'.$plant_details['logo']); ?>">
+			<?php }else{ ?>
+			<img src="<?php echo base_url('assets/plant_logo/noimage.jpg'); ?>">
+			<?php } ?>
+				
+			</div>
+			<div class="hosptal_name">
+				<?php echo $plant_details['disposal_plant_name']; ?> 
+				<div class="authorised">
+					(Authorised by <?php echo isset($plant_details['state'])?$plant_details['state']:' '; ?>. Pollution Control Board)
+				</div>
+			</div>
+			
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+			<div class="qr-code">
+				Bio-Medical Waste QR code
+			</div>
+				<div class="barcode-div" style="text-align:center" id="printThis<?php echo htmlentities($hospital_detail['h_id']); ?>">
+					<img style="" src="<?php echo base_url('assets/hospital_barcodes/'.$hospital_detail['barcode']);?>" alt="bar code" />
+				</div>
+				<div class="hosptal_name1">
+					<?php echo $hospital_detail['hospital_name']; ?> 
+					<?php echo ', '.$hospital_detail['city']; ?> 
+				</div>
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+				
+				<div class="address-section">
+				<?php echo isset($plant_details['address1'])?$plant_details['address1'].',':''; ?>
+				<?php echo isset($plant_details['address2'])?$plant_details['address2'].',':''; ?>
+				<?php echo isset($plant_details['city'])?$plant_details['city'].',':''; ?>
+				<?php echo isset($plant_details['state'])?$plant_details['state'].',':''; ?>
+				<?php echo isset($plant_details['country'])?$plant_details['country'].',':''; ?>
+				<?php echo isset($plant_details['pincode'])?$plant_details['pincode'].'.':''; ?>
+				</div>
+				<div class="address-section-con">
+				<?php echo isset($plant_details['mobile'])?$plant_details['mobile']:''; ?> | <?php echo isset($plant_details['email'])?$plant_details['email']:''; ?>
+				</div>
+			</div>
+		</div><div class="sticker-row1" style="width:8.5cm;height:9cm;border:1px solid #aaa;overflow:hidden;">
+			<div style="position: relative;">
+			<div class="hazard">
+				<img style="" src="<?php echo base_url('assets/vendor/images/biohazard.png');?>" alt="bar code" />
+			</div>
+			<div class="sticker-logo">
+			<?php if($plant_details['logo']!=''){ ?>
+			<img src="<?php echo base_url('assets/plant_logo/'.$plant_details['logo']); ?>">
+			<?php }else{ ?>
+			<img src="<?php echo base_url('assets/plant_logo/noimage.jpg'); ?>">
+			<?php } ?>
+				
+			</div>
+			<div class="hosptal_name">
+				<?php echo $plant_details['disposal_plant_name']; ?> 
+				<div class="authorised">
+					(Authorised by <?php echo isset($plant_details['state'])?$plant_details['state']:' '; ?>. Pollution Control Board)
+				</div>
+			</div>
+			
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+			<div class="qr-code">
+				Bio-Medical Waste QR code
+			</div>
+				<div class="barcode-div" style="text-align:center" id="printThis<?php echo htmlentities($hospital_detail['h_id']); ?>">
+					<img style="" src="<?php echo base_url('assets/hospital_barcodes/'.$hospital_detail['barcode']);?>" alt="bar code" />
+				</div>
+				<div class="hosptal_name1">
+					<?php echo $hospital_detail['hospital_name']; ?> 
+					<?php echo ', '.$hospital_detail['city']; ?> 
+				</div>
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+				
+				<div class="address-section">
+				<?php echo isset($plant_details['address1'])?$plant_details['address1'].',':''; ?>
+				<?php echo isset($plant_details['address2'])?$plant_details['address2'].',':''; ?>
+				<?php echo isset($plant_details['city'])?$plant_details['city'].',':''; ?>
+				<?php echo isset($plant_details['state'])?$plant_details['state'].',':''; ?>
+				<?php echo isset($plant_details['country'])?$plant_details['country'].',':''; ?>
+				<?php echo isset($plant_details['pincode'])?$plant_details['pincode'].'.':''; ?>
+				</div>
+				<div class="address-section-con">
+				<?php echo isset($plant_details['mobile'])?$plant_details['mobile']:''; ?> | <?php echo isset($plant_details['email'])?$plant_details['email']:''; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row loop-help" style="position:relative;">
+		<div class="sticker-row" style="width:8.5cm;height:9cm;border:1px solid #aaa;overflow:hidden;">
+			<div style="position: relative;">
+			<div class="hazard">
+				<img style="" src="<?php echo base_url('assets/vendor/images/biohazard.png');?>" alt="bar code" />
+			</div>
+			<div class="sticker-logo">
+			<?php if($plant_details['logo']!=''){ ?>
+			<img src="<?php echo base_url('assets/plant_logo/'.$plant_details['logo']); ?>">
+			<?php }else{ ?>
+			<img src="<?php echo base_url('assets/plant_logo/noimage.jpg'); ?>">
+			<?php } ?>
+				
+			</div>
+			<div class="hosptal_name">
+				<?php echo $plant_details['disposal_plant_name']; ?> 
+				<div class="authorised">
+					(Authorised by <?php echo isset($plant_details['state'])?$plant_details['state']:' '; ?>. Pollution Control Board)
+				</div>
+			</div>
+			
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+			<div class="qr-code">
+				Bio-Medical Waste QR code
+			</div>
+				<div class="barcode-div" style="text-align:center" id="printThis<?php echo htmlentities($hospital_detail['h_id']); ?>">
+					<img style="" src="<?php echo base_url('assets/hospital_barcodes/'.$hospital_detail['barcode']);?>" alt="bar code" />
+				</div>
+				<div class="hosptal_name1">
+					<?php echo $hospital_detail['hospital_name']; ?> 
+					<?php echo ', '.$hospital_detail['city']; ?> 
+				</div>
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+				
+				<div class="address-section">
+				<?php echo isset($plant_details['address1'])?$plant_details['address1'].',':''; ?>
+				<?php echo isset($plant_details['address2'])?$plant_details['address2'].',':''; ?>
+				<?php echo isset($plant_details['city'])?$plant_details['city'].',':''; ?>
+				<?php echo isset($plant_details['state'])?$plant_details['state'].',':''; ?>
+				<?php echo isset($plant_details['country'])?$plant_details['country'].',':''; ?>
+				<?php echo isset($plant_details['pincode'])?$plant_details['pincode'].'.':''; ?>
+				</div>
+				<div class="address-section-con">
+				<?php echo isset($plant_details['mobile'])?$plant_details['mobile']:''; ?> | <?php echo isset($plant_details['email'])?$plant_details['email']:''; ?>
+				</div>
+			</div>
+		</div><div class="sticker-row1" style="width:8.5cm;height:9cm;border:1px solid #aaa;overflow:hidden;">
+			<div style="position: relative;">
+			<div class="hazard">
+				<img style="" src="<?php echo base_url('assets/vendor/images/biohazard.png');?>" alt="bar code" />
+			</div>
+			<div class="sticker-logo">
+			<?php if($plant_details['logo']!=''){ ?>
+			<img src="<?php echo base_url('assets/plant_logo/'.$plant_details['logo']); ?>">
+			<?php }else{ ?>
+			<img src="<?php echo base_url('assets/plant_logo/noimage.jpg'); ?>">
+			<?php } ?>
+				
+			</div>
+			<div class="hosptal_name">
+				<?php echo $plant_details['disposal_plant_name']; ?> 
+				<div class="authorised">
+					(Authorised by <?php echo isset($plant_details['state'])?$plant_details['state']:' '; ?>. Pollution Control Board)
+				</div>
+			</div>
+			
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+			<div class="qr-code">
+				Bio-Medical Waste QR code
+			</div>
+				<div class="barcode-div" style="text-align:center" id="printThis<?php echo htmlentities($hospital_detail['h_id']); ?>">
+					<img style="" src="<?php echo base_url('assets/hospital_barcodes/'.$hospital_detail['barcode']);?>" alt="bar code" />
+				</div>
+				<div class="hosptal_name1">
+					<?php echo $hospital_detail['hospital_name']; ?> 
+					<?php echo ', '.$hospital_detail['city']; ?> 
+				</div>
+				<div style="border-bottom:1px solid #ddd;margin:0.1cm 0;"></div>
+				
+				<div class="address-section">
+				<?php echo isset($plant_details['address1'])?$plant_details['address1'].',':''; ?>
+				<?php echo isset($plant_details['address2'])?$plant_details['address2'].',':''; ?>
+				<?php echo isset($plant_details['city'])?$plant_details['city'].',':''; ?>
+				<?php echo isset($plant_details['state'])?$plant_details['state'].',':''; ?>
+				<?php echo isset($plant_details['country'])?$plant_details['country'].',':''; ?>
+				<?php echo isset($plant_details['pincode'])?$plant_details['pincode'].'.':''; ?>
+				</div>
+				<div class="address-section-con">
+				<?php echo isset($plant_details['mobile'])?$plant_details['mobile']:''; ?> | <?php echo isset($plant_details['email'])?$plant_details['email']:''; ?>
+				</div>
+			</div>
+		</div>
 	</div>
 	
 	
