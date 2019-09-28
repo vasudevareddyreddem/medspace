819001b7e38e69ebbb997be454183b1cffa0a212
bayapucg <bayapu.edu004@gmail.com>
2019-09-28 12:32:19 +0530

    commit
    bor

--- application/views/admin/100x50.php
+++ application/views/admin/100x50.php
@@ -33,7 +33,7 @@
 	<?php if(isset($print_details) && count($print_details)>0){ ?>
 	<?php foreach($print_details as $list){ ?>
 	<div style="display:flex;justify-content: center;">
-	   <div class="sticker" style="width:50mm;height:50mm;background:#fff;overflow:hidden;position:relative;margin:3mm 0mm 0mm 0mm;border-radius:5px">
+	   <div class="sticker" style="width:50mm;height:50mm;background:#fff;overflow:hidden;position:relative;margin:3mm 0mm 0mm 0mm;border-radius:5px;border:0.5px solid #ddd;">
 		  <div style="">
 			 <div >
 				<div style="padding:0px;margin-left:8mm;margin-top:1mm;font-size:16px;height:10mm">Seq.No.<?php echo $list[0]['ptcnt'] ; ?> </span></div>
@@ -42,14 +42,14 @@
 				<div>
 				   <img style="width:auto;height:30mm;padding:2px 0px;" src="<?php echo base_url('assets/hospital_waste_barcodes/'.$list[0]['barcode']); ?>" alt="<?php echo $list[0]['barcode'] ; ?>" >
 				</div>
-				<div style="position:absolute;top:1mm;left:1mm" class="<?php echo $list[0]['category'] ; ?>-col"></div>
+				<div style="position:absolute;top:3mm;left:3mm" class="<?php echo $list[0]['category'] ; ?>-col"></div>
 			 </div>
 			 <div >
 				<div style="padding:0px;margin:0px;font-size:15px"><?php echo $list[0]['barcodetext'] ; ?></span></div>
 			 </div>
 		  </div>
 	   </div>
-	   <div class="sticker" style="width:50mm;height:50mm;background:#fff;overflow:hidden;position:relative;margin:3mm 0mm 0mm 0mm;border-radius:5px">
+	   <div class="sticker" style="width:50mm;height:50mm;background:#fff;overflow:hidden;position:relative;margin:3mm 0mm 0mm 0mm;border-radius:5px;border:0.5px solid #ddd;">
 		  <div style="">
 			 <div >
 				<div style="padding:0px;margin-left:8mm;margin-top:1mm;font-size:16px;height:10mm">Seq.No.<?php echo $list[1]['ptcnt'] ; ?> </span></div>
@@ -58,7 +58,7 @@
 				<div>
 				   <img style="width:auto;height:30mm;padding:2px 0px;" src="<?php echo base_url('assets/hospital_waste_barcodes/'.$list[1]['barcode']); ?>" alt="<?php echo $list[1]['barcode'] ; ?>" >
 				</div>
-				<div style="position:absolute;top:1mm;left:1mm" class="<?php echo $list[1]['category'] ; ?>-col"></div>
+				<div style="position:absolute;top:3mm;left:3mm" class="<?php echo $list[1]['category'] ; ?>-col"></div>
 			 </div>
 			 <div >
 				<div style="padding:0px;margin:0px;font-size:15px"><?php echo $list[1]['barcodetext'] ; ?></span></div>
